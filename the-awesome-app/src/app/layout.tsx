37fd858bfcb2da332f42806cec135f87cf82e32b
aniljos <anil.jos@gmail.com>
2025-02-25 12:27:09 +0530

    Day 2 session 1

--- the-awesome-app/src/app/layout.tsx
+++ the-awesome-app/src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import AppBar from "@/components/AppBar";
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,7 +27,17 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        {children}
+
+        <div className="container">
+          
+          <AppBar/>
+
+          <main>
+            {children}
+          </main>
+
+        </div>
+
       </body>
     </html>
   );
--- the-awesome-app/src/app/login/page.tsx
+++ the-awesome-app/src/app/login/page.tsx
@@ -1,9 +1,58 @@
-export default function LoginPage(){
+'use client'
 
+import { ChangeEvent, FormEvent, useState } from "react"
+
+export default function LoginPage() {
+
+    const [name, setName]  = useState(""); 
+    const [password, setPassword] = useState("");
+    const [message, setMessage]  = useState("")
+    
+
+    function handleLogin(evt: FormEvent<HTMLFormElement>){
+         evt.preventDefault();
+
+         // clients-side validation
+         if(name && password){
+                //validate the credential(server)
+
+                setMessage("");
+         }
+         else{
+            //alert("Enter the credentials...");
+            setMessage("Enter the credentials");
+         }
+         
+
+    }
+    function handleNameChange(evt: ChangeEvent<HTMLInputElement>){
+
+        //const value = evt.target.value;
+        //setName(value);
+        setName(evt.target.value);
+    }
     return (
         <div>
             <h3>Login</h3>
             <p>Signin to your application</p>
+
+            {message ? <div className="alert alert-warning">{message}</div> : null}
+
+            <form onSubmit={handleLogin}>
+                <div className="form-group">
+                    <label htmlFor="name">Name</label>
+                    <input type="text" className="form-control" id="name" value={name} onChange={handleNameChange}/>
+                </div>
+
+                <div className="form-group">
+                    <label htmlFor="pwd">Password</label>
+                    <input type="password" className="form-control" id="pwd" 
+                                                            value={password} onChange={evt => setPassword(evt.target.value)}/>
+                </div>
+
+                <br />
+                <button className="btn btn-success">Login</button>
+            </form>
         </div>
     )
 }
